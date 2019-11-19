9f40971b2d97067302cc2748e49c85350edc7497
User <User@DESKTOP-083QOIL>
2019-11-19 21:58:46 +0900

    초기 커밋.

---
+++ Project21/소스.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNIGNS
+
+#include <stdio.h>
+
+int main(void)
+{
+	int data1 = 10;
+	int data2 = 20;
+	
+	int sum = 0;
+
+	int *p = &data1;
+	*p = 100;
+	sum = sum + *p;
+
+	p = &data2;
+	*p = 200;
+	sum = sum + *p;
+
+	printf("sum = %d\n", sum);
+}
